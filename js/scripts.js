60add2178bb485bebaef8066a07fe4e84b8617c2
Jordan Meier and Kevin Mattison <mattisonii@yahoo.com>
2016-01-19 13:59:31 -0800

    Add functionality to user interface that accounts for user entering NaNs,such as strings, and decimal values

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //BDD Red, Green, Refactor for business logic testing
 var triangleTracker = function (side1, side2, side3) {
-  
+
   if ((side1 + side2 <= side3)) {
     return false;
   } else if ((side1 === side2) && (side2 === side3)) {
@@ -23,25 +23,29 @@ $(document).ready(function() {
 
     if ((side1.length === 0) || (side2.length === 0) || (side3.length === 0)) {
       alert("Please fill in all three values.");
+//We want a different alert to pop up if user enters a NaN
+    } else if ((isNaN(side1)) || (isNaN(side2)) || (isNaN(side3))) {
+      alert("Please fill in only numeric values.");
     } else {
-
-      var side1 = parseInt($("input#side1").val());
-      var side2 = parseInt($("input#side2").val());
-      var side3 = parseInt($("input#side3").val());
+// The following conditional statements determine what type of triangle it is. parseFloat is added because at this point, values should be numeric, and parseInt will not work on decimal nubmers.
+      var side1 = parseFloat($("input#side1").val());
+      var side2 = parseFloat($("input#side2").val());
+      var side3 = parseFloat($("input#side3").val());
 
       if ((side2 + side3 <= side1) || (side3 + side1 <= side2) || (side1 + side2 <= side3)) {
         $(".triangleType").text("are not a");
       } else if ((side1 === side2) && (side2 === side3)) {
         $(".triangleType").text("produces an equilateral");
       } else if ((side1 === side2) && (side2 !== side3) || (side2 === side3) && (side3 !== side1) || (side3 === side1) && (side1 !== side2)) {
-        $(".triangleType").text("produces a isosceles");
+        $(".triangleType").text("produces an isosceles");
       } else if ((side1 !== side2) && (side2 !== side3) || (side3 !== side1)) {
         $(".triangleType").text("produces a scalene");
       }
+      $("#result").show();
     }
 
 
-  $("#result").show();
+  // $("#result").show();
   event.preventDefault();
   });
 });
