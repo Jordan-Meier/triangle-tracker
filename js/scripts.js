a8ea71ef1a9be9b9112c2596c5c0e09f1b958456
Jordan Meier and Kevin Mattison <jbirdmeier@gmail.com>
2016-01-19 10:56:35 -0800

    Delete commented out pre-refactored code in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-// var triangleTracker = function (side1, side2, side3) {
-//   if ((side1 === side2) && (side2 === side3)) {
-//     return true;
-//   } else if ((side1 === side2) && (side2 !== side3)) {
-//     return true;
-//   } else if ((side1 !== side2) && (side2 !== side3)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+
 
 
 var triangleTracker = function (side1, side2, side3) {
